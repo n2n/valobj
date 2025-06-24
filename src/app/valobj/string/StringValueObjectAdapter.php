3a4b76715c51b781d4370d70fa5ff86038e36a04
david <meier@hnm.ch>
2025-06-24 14:28:30 +0200

    equals() for StringValueObjectAdapter

--- src/app/valobj/string/StringValueObjectAdapter.php
+++ src/app/valobj/string/StringValueObjectAdapter.php
@@ -7,6 +7,10 @@ use n2n\util\ex\ExUtils;
 
 abstract class StringValueObjectAdapter implements StringValueObject, \Stringable, \JsonSerializable {
 
+	function equals(StringValueObject $stringValueObject): bool {
+		return $this->toScalar() === $stringValueObject->toScalar();
+	}
+
 	public function __toString(): string {
 		return $this->toScalar();
 	}
---
+++ src/test/valobj/impl/string/StringValueObjectAdapterTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace valobj\impl\string;
+
+use PHPUnit\Framework\TestCase;
+use n2n\spec\valobj\err\IllegalValueException;
+use valobj\ValueObjects;
+use n2n\bind\build\impl\Bind;
+use n2n\bind\mapper\impl\Mappers;
+use n2n\bind\err\BindTargetException;
+use n2n\bind\err\BindMismatchException;
+use n2n\bind\err\UnresolvableBindableException;
+use n2n\validation\plan\ErrorMap;
+use valobj\string\Email;
+
+class StringValueObjectAdapterTest extends TestCase {
+
+	function testEquals(): void {
+		$this->assertTrue((new Email('holeradio@huii.ch'))->equals(new Email('holeradio@huii.ch')));
+		$this->assertFalse((new Email('holeradio@super-huii.ch'))->equals(new Email('holeradio@huii.ch')));
+	}
+}
