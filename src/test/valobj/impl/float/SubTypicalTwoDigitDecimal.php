8a3cccf4081b720037347a6fb360263fd65b0407
Andreas von Burg <von-burg@hnm.ch>
2025-08-29 01:27:05 +0200

    TypicalTwoDigitDecimal

--- src/test/valobj/impl/float/SubTypicalTwoDigitDecimal.php
+++ src/test/valobj/impl/float/SubTypicalTwoDigitDecimal.php
@@ -3,7 +3,6 @@
 namespace valobj\impl\float;
 
 use valobj\float\TypicalTwoDigitDecimal;
-use valobj\float\FloatValueObjectAdapter;
 
 class SubTypicalTwoDigitDecimal extends TypicalTwoDigitDecimal {
 
