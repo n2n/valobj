00c88d844192b13ab0b004dc39b9babf4baa5bbf
andreas <a@von-burg.net>
2025-11-04 15:02:27 +0100

    PositiveInt

--- src/app/valobj/int/NbId.php
+++ src/app/valobj/int/NbId.php
@@ -8,22 +8,6 @@ use n2n\bind\mapper\Mapper;
 use n2n\bind\mapper\impl\Mappers;
 use n2n\bind\attribute\impl\Unmarshal;
 
-class NbId extends IntValueObjectAdapter {
-	public function __construct(int $value) {
-		parent::__construct($value);
-		IllegalValueException::assertTrue($this->value > 0,
-				'Value too small: ' . $this->value);
-	}
+class NbId extends PositiveInt {
 
-	#[Marshal]
-	static function marshalMapper(): Mapper {
-		return Mappers::valueClosure(fn (NbId $nbId) => $nbId->toScalar());
-	}
-
-	#[Unmarshal]
-	static function unmarshalMapper(): Mapper {
-		$class = new \ReflectionClass(static::class);
-		return Mappers::pipe(Mappers::int(false, 1, PHP_INT_MAX),
-				Mappers::valueNotNullClosure(fn (int $id) => $class->newInstance($id)));
-	}
 }
