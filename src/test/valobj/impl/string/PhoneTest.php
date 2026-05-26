859c7abe52fd13fcd55e6382bb3be6cd317ef9c5
david <meier@hnm.ch>
2026-05-26 16:48:04 +0200

    Phone value object allows () in lenient mode

--- src/test/valobj/impl/string/PhoneTest.php
+++ src/test/valobj/impl/string/PhoneTest.php
@@ -8,60 +8,44 @@ use valobj\string\Phone;
 
 class PhoneTest extends TestCase {
 	function testLenientFrom(): void {
-		$this->assertEquals('+49 (0)228-997799-0', Phone::from('+49 (0)228-997799-0', true));
-		$this->assertEquals('+49 (0)228-997799-0', Phone::from(' +49 (0)228-997799-0 ', true));
-		$this->assertEquals('+49 (0)228-997799-0', Phone::from('0049 (0)228-997799-0', true));
-		$this->assertEquals('+49 228-997799-0', Phone::from('0049 228-997799-0', true));
-		$this->assertEquals('0228-997799-0', Phone::from('0228-997799-0', true));
-		$this->assertEquals('+49(0)228-997799-0', Phone::from('+49(0)228-997799-0', true));
-		$this->assertEquals('+900 (0)228-997799-0', Phone::from('+900 (0)228-997799-0', true));
-		$this->assertEquals('+900(0)228-997799-0', Phone::from('+900(0)228-997799-0', true));
-		$this->assertEquals('+41(0)79 123 45 67', Phone::from('+41(0)79 123 45 67', true));
-		$this->assertEquals('079 1 2 3 45 67', Phone::from('  079  1 2 3  45  67  ', true));
+		$this->assertSame('+49 (0)228-997799-0', Phone::from('+49 (0)228-997799-0', true)->toScalar());
+		$this->assertSame('+49 (0)228-997799-0', Phone::from(' +49 (0)228-997799-0 ', true)->toScalar());
+		$this->assertSame('+49 (0)228-997799-0', Phone::from('0049 (0)228-997799-0', true)->toScalar());
+		$this->assertSame('+49 228-997799-0', Phone::from('0049 228-997799-0', true)->toScalar());
+		$this->assertSame('0228-997799-0', Phone::from('0228-997799-0', true)->toScalar());
+		$this->assertSame('+49(0)228-997799-0', Phone::from('+49(0)228-997799-0', true)->toScalar());
+		$this->assertSame('+900 (0)228-997799-0', Phone::from('+900 (0)228-997799-0', true)->toScalar());
+		$this->assertSame('+900(0)228-997799-0', Phone::from('+900(0)228-997799-0', true)->toScalar());
+		$this->assertSame('+41(0)79 123 45 67', Phone::from('+41(0)79 123 45 67', true)->toScalar());
+		$this->assertSame('079 1 2 3 45 67', Phone::from('  079  1 2 3  45  67  ', true)->toScalar());
+		$this->assertSame('0228-997799-0', Phone::from('(0)228-997799-0', true)->toScalar());
+		$this->assertSame('+49 10228-997799-0', Phone::from('+49 (1)0228-997799-0', true)->toScalar());
+		$this->assertSame('+900 1228-0997799-0', Phone::from('+900 1228-(0)997799-0', true)->toScalar());
+		$this->assertEquals('+900(0)122899977990', Phone::from('+900(0)122899977990', true));
+
+		$this->assertSame('+12340791234567', Phone::from('+1234(0)791234567', true)->toScalar());
+		$this->assertSame('+0791234567', Phone::from('+ (0)791234567', true)->toScalar());
+
 	}
 
 	function testToTel(): void {
 		$this->assertEquals('+492289977990', Phone::from('+49 (0)228-997799-0', true)->toTel());
 	}
 
-	function testFailPhone1(): void {
-		$this->expectException(IllegalStateException::class);
-		$this->assertEquals('0228-997799-0', Phone::from('(0)228-997799-0', true));
-	}
-
-	function testFailPhone2(): void {
+	function testFailPhoneBecausePlusOnWrongPlace(): void {
 		$this->expectException(IllegalStateException::class);
 		$this->assertEquals('0228997799-0', Phone::from('0228+997799-0', true));
 	}
 
-	function testFailPhone3(): void {
+	function testFailPhoneBecauseIllegalChars(): void {
 		$this->expectException(IllegalStateException::class);
-		$this->assertEquals('+49 1228-997799-0', Phone::from(' +49 (1)22+8-997799-0 ', true));
+		$this->assertEquals('+49 1228-997799-0', Phone::from(' tel: +49 228-997799-0 ', true));
 	}
 
-	function testFailPhone4(): void {
-		$this->expectException(IllegalStateException::class);
-		$this->assertEquals('+49 10228-997799-0', Phone::from('+49 (1)0228-997799-0', true));
-	}
 
-	function testFailPhone5(): void {
+	function testFailPhoneBecauseToLong(): void {
 		$this->expectException(IllegalStateException::class);
-		$this->assertEquals('+900 1228-0997799-0', Phone::from('+900 1228-(0)997799-0', true));
-	}
-
-	function testFailPhone6(): void {
-		$this->expectException(IllegalStateException::class);
-		$this->assertEquals('+9001228099977990', Phone::from('+9001228(0)99977990', true));
-	}
-
-	function testFailPhone7(): void {
-		$this->expectException(IllegalStateException::class);
-		$this->assertEquals('0791234567', Phone::from('+ (0)791234567', true));
-	}
-
-	function testFailPhone8(): void {
-		$this->expectException(IllegalStateException::class);
-		$this->assertEquals('0791234567', Phone::from('+1234(0)791234567', true));
+		$this->assertEquals('+9001228099977990', Phone::from('+9001228099977990', true));
 	}
 
 	function testNotLenientFrom(): void {
